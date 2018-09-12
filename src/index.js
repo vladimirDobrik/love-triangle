fd32d383c386ed55013b231fcd52d847dfbff11a
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-09-12 19:41:25 +0300

    love-triangle/fix: fixed func getLoveTrianglesCount in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    var loveTrianglesCounter = 0;
+
+    for (var i = 0; i < preferences.length; i++) {
+
+        var lover = preferences[i] - 1;
+        var nextLover = preferences[lover] - 1;
+        var nextNextLover = preferences[nextLover] - 1;
+
+        if (nextNextLover == i) loveTrianglesCounter++;
+    }
+
+    if (loveTrianglesCounter % 3) {
+        return Math.floor(loveTrianglesCounter / 3);
+    } else {
+        return loveTrianglesCounter / 3;
+    }
 };
